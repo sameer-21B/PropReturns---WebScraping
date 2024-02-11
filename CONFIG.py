b1e0bf824ff0e11efb9533ebfdda661ac1d6e265
Sameer Bhosale <56029975+sameer-21B@users.noreply.github.com>
2024-02-11 21:20:22 +0530

    Add files via upload

---
+++ CONFIG.py
@@ -0,0 +1,8 @@
+config={
+    'PRIMARY_URL':'https://pay2igr.igrmaharashtra.gov.in/eDisplay/Propertydetails/index',
+    'YEAR':'2023',
+    'DISTRICT':'मुंबई उपनगर',
+    'TALUKA':'अंधेरी',
+    'VILLAGE':'बांद्रा',
+    'INP_FREE_TXT':'2023'
+}
